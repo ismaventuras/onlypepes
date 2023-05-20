b56a86e028d2d6fc748056d55f3e246a8c5f10ec
Ismael Bautista <ismaelbautista6@gmail.com>
2023-05-20 14:57:56 +0200

    moved bash scripts to separate folder

---
+++ scripts/install-nodejs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install curl
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
+nvm install node 
