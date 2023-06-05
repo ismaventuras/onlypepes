252e673083b671dd30e9857a5abfb008d333aa36
Ismael Bautista <ismaelbautista6@gmail.com>
2023-06-05 12:49:57 +0200

    lmao

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images:{
-<<<<<<< HEAD:apps/web/next.config.js
         domains:['cdn.onlypepes.xyz']
-=======
-        domains:['cdn.onlypepes.xyz']
->>>>>>> d9781ab04cd8f1a83bd5a2f723dc9c605965e79a:next.config.js
     }
 }
 
